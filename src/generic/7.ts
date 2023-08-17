7987d89c036fbdf023a849842d04b37aa9b93fe0
Anastasia Koptilova <koptilova.nastya@gmail.com>
2023-08-17 18:55:57 +0300

    done all tasks

--- src/generic/7.ts
+++ src/generic/7.ts
@@ -8,9 +8,9 @@ export enum UserRole {
   editor = 'editor',
   guest = 'guest',
 }
-
+type Roles = Record<UserRole, string>;
 // Замініть наступний код на версію за допомогою Record
-const RoleDescription = {
+const RoleDescription: Roles = {
   admin: 'Admin User',
   editor: 'Editor User',
   guest: 'Guest User',
